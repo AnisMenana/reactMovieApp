6db734ab7e98c64c232f317e147caf170211ae98
Menana <anismenana@gmail.com>
2019-07-24 18:20:15 +0100

    test2

--- src/App.js
+++ src/App.js
@@ -10,11 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org">
           Learn React
         </a>
       </header>
