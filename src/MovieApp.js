5fa84348688672f079f212787021bfa1376769d6
Menana <anismenana@gmail.com>
2019-07-24 23:26:00 +0100

    added movieApp

---
+++ src/MovieApp.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class MovieApp extends Component {
+  state = {
+    searchMovie: "",
+    rate: 0,
+    movie: []
+  };
+  render() {
+    return <div />;
+  }
+}
+
+export default MovieApp;
