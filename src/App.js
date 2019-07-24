9d26dd2ab85f288f71c2fb9c6127d8db77438cd3
Menana <anismenana@gmail.com>
2019-07-24 18:21:58 +0100

    test3

--- src/App.js
+++ src/App.js
@@ -3,19 +3,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org">
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="App" />;
 }
 
 export default App;
