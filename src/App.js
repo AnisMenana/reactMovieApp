230c97a93ff18db33a20827b7a77df2b80b71e20
Menana <anismenana@gmail.com>
2019-07-24 18:13:35 +0100

    test

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -13,7 +13,6 @@ function App() {
         <a
           className="App-link"
           href="https://reactjs.org"
-          target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
